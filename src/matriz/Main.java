0010d23e56a3c358eecd523c92b04ed5ddd20d1a
lykan3 <josemiguel.cardenasmorales@unitecnar.edu.co>
2025-04-12 15:54:27 -0500

    carga inicial

---
+++ src/matriz/Main.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matriz;
+
+/**
+ *
+ * @author LYKAN
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // matriz llena
+    int [][] matriz ={
+        {10,20,30,40,50},
+        {60,70,80,90,100},
+        {120,130,140,150,160},
+        {170,180,190,200,210},
+        {220,230,240,250,260},
+    };
+        for (int f = 0; f <matriz.length; f++) {
+            for (int c = 0; c < matriz.length; c++) {
+                if(f==0 && c==0){
+                    System.out.println("la posicion de f es:"+f+"la pocicion de c es:"+c+"el valor es:"+matriz[f][c]);   
+                }else if(f==0 && c==4){
+                    System.out.println("la posicion de f es:"+f+"la pocicion de c es:"+c+"el valor es:"+matriz[f][c]);
+                }else if(f==1 && c==1){
+                    System.out.println("la posicion de f es:"+f+"la pocicion de c es:"+c+"el valor es:"+matriz[f][c]);
+                } else if(f==1 && c==3){  
+                    System.out.println("la posicion de f es:"+f+"la pocicion de c es:"+c+"el valor es:"+matriz[f][c]);
+                } else if(f==2 && c==2){  
+                    System.out.println("la posicion de f es:"+f+"la pocicion de c es:"+c+"el valor es:"+matriz[f][c]);
+                }else if(f==3 && c==1){  
+                    System.out.println("la posicion de f es:"+f+"la pocicion de c es:"+c+"el valor es:"+matriz[f][c]);
+                }
+            }
+            
+        }
+    }
+    
+}
